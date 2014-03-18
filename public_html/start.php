944fb98ef21dc2096417013a4d17baf737ff7902
Ben Meier <benmeier42@gmail.com>
2014-03-18 17:10:23 +0200

    fixed stuff

--- public_html/start.php
+++ public_html/start.php
@@ -5,19 +5,6 @@ ini_set('display_errors', 1);
 
 include 'config.php';
 
-// long running process unix/windows
-function execInBackground($cmd)
-{
-    if (substr(php_uname(), 0, 7) == "Windows")
-    {
-        pclose(popen("start /B ". $cmd, "r"));
-    }
-    else
-    {
-        exec($cmd . " > /dev/null &");
-    }
-}
-
 // check iso date
 $from = new DateTime($_GET["from"]);
 $to = new DateTime($_GET["to"]);
@@ -27,7 +14,7 @@ $from->setTimeZone(new DateTimeZone('UTC'));
 $to->setTimeZone(new DateTimeZone('UTC'));
 
 // first check from < to
-if(from >= to)
+if($from >= $to)
 {
     echo '{"status":1, "error": "From date must be before To date."}';
 }
@@ -46,18 +33,20 @@ else
         // query table
         $result = mysqli_query($con, "SELECT * FROM ir_update_state WHERE state='running' ORDER BY ts DESC;");
 
-        $running = mysql_num_rows($result);
+        $running = $result->num_rows;
         if($running > 0)
         {
-            $first = $row = mysqli_fetch_array($result)
+            $first = $row = mysqli_fetch_array($result);
             echo '{"status":1, "error": "There is already an update job running [' . $row['pid'] . ']."}';
         }
         else
         {
 
             $cmd = "ruby " . $update_script . " '" . $from->format('Y-m-d\Th:i:s\Z') . "' '" . $to->format('Y-m-d\Th:i:s\Z') . "'";
-            execInBackground($cmd);
 
+            $out = exec($cmd . " > /dev/null &");
+
+            echo $out;
 
         }
     }
